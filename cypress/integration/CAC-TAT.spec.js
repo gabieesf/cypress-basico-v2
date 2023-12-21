73fda9813195cdf95447792c94bdb39ce796f575
Gabriela <gabrieladesousaf@hotmail.com>
2023-12-21 16:18:52 -0300

    atualização de código

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -148,12 +148,12 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .should('have.attr','target','_blank')
     })
 
-    it.only('acessa a página da política de privacidade removendo o target e então clicando no link', function(){
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', function(){
         cy.get('#privacy > a').invoke('removeAttr', 'target')
         .should('not.have.attr','target')
     })
 
-    
+
 
 
     
